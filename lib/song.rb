aa75a040d03fe167401d17803ce4e13e254b0bb9
Keesha Hargrove <kharg14@gmail.com>
2020-08-04 00:16:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,27 @@
-class Song
-  attr_accessor :artist, :name
+class Song 
+  attr_accessor :name, :artist
+
+  @@all = []
 
   def initialize(name)
-    @name = name
+    self.name = name
+    @@all << self
+  end 
+
+  def self.all 
+    @@all
+  end 
+
+  def self.new_by_filename(file)
+    song = file.split(" - ")
+    new_song = Song.new(song[1])
+    new_song.artist_name=(song[0])
+    new_song
   end
 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+  end 
 
-  def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song.artist_name = song_info[0]
-    song
-  end
-end
+end 
